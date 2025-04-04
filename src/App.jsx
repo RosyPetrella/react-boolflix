ec7fc80955101ad5589fbd82a239d04274c2e4ee
RosyPetrella <petrellamr@gmail.com>
2025-04-04 12:10:46 +0200

    Implement basic layout with search functionality for movie queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
-export default function App() {
+// Milestone 0:
+// Progettare la struttura del global state sulla linea degli esercizi svolti nei giorni
+// precedenti.
+
+// Milestone 1:
+// Creare un layout base con una searchbar (una input e un button) in cui possiamo
+// scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il
+// bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni
+// film trovato:
+// 1.  Titolo
+// 2.  Titolo Originale
+// 3.  Lingua
+// 4.  Voto
+
+import { useState } from "react";
+
+function App() {
+  // Stato per memorizzare il valore dell'input
+  const [query, setQuery] = useState("");
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div>
+      <h1>React Boolflix</h1>
+      <div>
+        {/* nome film */}
+        <input
+          type="text"
+          placeholder="Cerca un film..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button onClick={() => console.log("Cerco:", query)}>Cerca</button>
+      </div>
+    </div>
   );
 }
+
+export default App;
