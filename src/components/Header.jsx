1fa62092c1f7721c063a36609c54107294077478
RosyPetrella <petrellamr@gmail.com>
2025-04-07 12:06:34 +0200

    Edit app structure by adding Header and SearchBar components; implement search functionality for movies and series

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import SearchBar from "./SearchBar";
+
+export default function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <h1 className="logo">
+          <a className="navbar-brand" href="#">
+            Boolflix
+          </a>
+        </h1>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse"
+          id="navbarSupportedContent"
+        ></div>
+        <div>
+          <SearchBar />
+        </div>
+      </div>
+    </nav>
+  );
+}
